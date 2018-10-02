c521fcc19492c0d5508fba5c569bb08f47e03584
David Arnold <dja+djaboxx@sonic.net>
2018-10-02 14:03:33 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "eks" {
 
 module "network" {
   source  = "app.terraform.io/Darnold-Pipeline/network/aws"
-  version = "3.0.2"
+  version = "3.0.3"
   key_name = "${var.key_name}"
   network_name = "${var.network_name}"
   region = "${var.region}"
