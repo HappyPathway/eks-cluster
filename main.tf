3f700807856f2d74da91ef068bf33e52a173eb36
David Arnold <dja+djaboxx@sonic.net>
2018-10-02 16:06:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,113 @@
-//--------------------------------------------------------------------
-// Variables
 variable "cluster_name" {}
-variable "availability_zone" {}
-variable "key_name" {}
-variable "network_name" {}
-variable "region" {}
+variable "env" {}
 
-
-//--------------------------------------------------------------------
-// Modules
-module "eks" {
-  source  = "app.terraform.io/Darnold-Pipeline/eks/aws"
-  version = "1.0.0"
-
-  cluster_name = "${var.cluster_name}"
-  subnets = ["${module.network.public_subnets}"]
-  vpc_id = "${module.network.vpc_id}"
-  manage_aws_auth = false
+terraform {
+  required_version = ">= 0.11.7"
 }
 
-module "network" {
-  source  = "app.terraform.io/Darnold-Pipeline/network/aws"
-  version = "3.0.8"
-  key_name = "${var.key_name}"
-  network_name = "${var.network_name}"
-  region = "${var.region}"
+provider "random" {
+  version = "= 1.3.1"
 }
 
-resource "random_id" "random" {
-  keepers {
-    uuid = "${uuid()}"
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "${var.cluster_name}-eks-${random_string.suffix.result}"
+
+  
+
+  worker_groups = [
+    {
+      instance_type       = "t2.small"
+      subnets             = "${join(",", module.vpc.private_subnets)}"
+    },
+    {
+      instance_type                 = "t2.small"
+      subnets                       = "${join(",", module.vpc.private_subnets)}"
+      additional_security_group_ids = "${aws_security_group.worker_group_mgmt_one.id},${aws_security_group.worker_group_mgmt_two.id}"
+    },
+  ]
+  tags = {
+    Environment = "${var.env}"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+    Workspace   = "${terraform.workspace}"
   }
-  byte_length = 32
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  description = "SG to be applied to all *nix machines"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
+
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+module "vpc" {
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "1.14.0"
+  name               = "${var.cluster_name}-vpc"
+  cidr               = "10.0.0.0/16"
+  azs                = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}", "${data.aws_availability_zones.available.names[2]}"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags               = "${merge(local.tags, map("kubernetes.io/cluster/${local.cluster_name}", "shared"))}"
+}
+
+module "eks" {
+  source                               = "github.com/terraform-aws-modules/terraform-aws-eks"
+  cluster_name                         = "${local.cluster_name}"
+  subnets                              = ["${module.vpc.private_subnets}"]
+  tags                                 = "${local.tags}"
+  vpc_id                               = "${module.vpc.vpc_id}"
+  worker_groups                        = "${local.worker_groups}"
+  worker_group_count                   = "2"
+  worker_additional_security_group_ids = ["${aws_security_group.all_worker_mgmt.id}"]
 }
